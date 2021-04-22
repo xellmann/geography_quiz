4b4dd4f46409dc612e32c1049523e5e0501c68f2
Ivo Gsellmann <ivo.gsellmann@gmx.at>
2021-04-22 07:07:54 +0200

    fixed a bug where right answers were handled like wrong ones

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from functions import *
 
 capitals = load_capitals()
+capitals_items = capitals.items()
 
 randomcity = game_data(capital_data=capitals)[0]
 correct_capital = game_data(capital_data=capitals)[1]
 
 while True:
-    print("What is the capital of {0}?".format(str(list(list(capitals.items())[randomcity])[0])))
+    print("What is the capital of {0}?".format(str(list(list(capitals_items)[randomcity])[0])))
     answer = input("Insert answer: ")
     if answer == correct_capital:
         print("You gooooot it! Good job!")
